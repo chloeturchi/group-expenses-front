298ed7ab75858d14fa898e635f50980714b2f59e
Thibault Dessarps <dessarps.thibault@gmail.com>
2020-09-29 22:44:01 +0200

    :whale: first remove volumes to avoid some conflicts

--- start-backend.sh
+++ start-backend.sh
@@ -2,4 +2,5 @@
 set -e
 
 curl https://raw.githubusercontent.com/Monsieur-Wary/group-expenses-back/master/docker-compose.yml -LsO
+docker-compose down --remove-orphans -v
 docker-compose pull && docker-compose up
